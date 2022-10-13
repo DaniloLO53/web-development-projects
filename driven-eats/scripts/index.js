72037b68ee1fb376939d60eacdeb53d6c55bc517
Danilo Leão <olin_ad@hotmail.com>
2022-10-13 15:54:49 -0300

    Adicionei filtros para as bebidas e sobremesas

--- driven-eats/scripts/index.js
+++ driven-eats/scripts/index.js
@@ -1,3 +1,137 @@
+import { data } from '../data.js';
+
+function drinkFilterHandle({ target }) {
+  const { value } = target;
+  const tagsSelector = {
+    'Filtrar': '',
+    'Não Alcólico': '',
+    'Cerveja': 'cerveja',
+    'Vinhos': 'vinho',
+    'Vinho Tinto': 'tinto',
+    'Vinho Branco': 'branco',
+  };
+  const tag = tagsSelector[value];
+
+  const oldCards = document.querySelectorAll(`[data-row="${1}"]`);
+  if (oldCards.length > 0) Array.from(oldCards).map((card) => card.remove());
+
+  cardCreator(tag, 1);
+}
+
+function dessertFilterHandle({ target }) {
+  const { value } = target;
+  const tagsSelector = {
+    'Filtrar': '',
+    'Chocolate': 'chocolate',
+    'Mousses': 'mousse',
+    'Bolos': 'bolo',
+    'Pudins': 'pudim',
+    'Doces': 'doce',
+  };
+  const tag = tagsSelector[value];
+
+  const oldCards = document.querySelectorAll(`[data-row="${2}"]`);
+  if (oldCards.length > 0) Array.from(oldCards).map((card) => card.remove());
+  cardCreator(tag, 2);
+}
+
+document.querySelector('#drinkFilters').addEventListener('click', drinkFilterHandle);
+document.querySelector('#dessertFilters').addEventListener('click', dessertFilterHandle);
+
+const activeClose = () => {
+  const choosed = Array.from(document.querySelectorAll('.choosed'));
+  const close = document.querySelector('.close');
+  const maxChoosed = 3;
+  if (choosed.length === maxChoosed) {
+    close.firstElementChild.removeAttribute('disabled');
+    close.firstElementChild.innerHTML = 'Fechar pedido';
+    close.firstElementChild.classList.add('closeActive');
+  } else {
+    close.firstElementChild.setAttribute('disabled', '');
+  }
+};
+
+const cardClickHandle = () => {
+  // transforma um HTMLCollection em algo tipo um array
+  // https://chuckdries.medium.com/traversing-the-dom-with-filter-map-and-arrow-functions-1417d326d2bc
+  const cards = Array.from(document.querySelectorAll('.card__mask'));
+
+  cards.map((card) => card.addEventListener('click', (event) => {
+    // lógica para os filhos da div não herdarem o evento
+    if (event.target === event.currentTarget) {
+      if (Array.from(document.querySelectorAll('.choosed')).length > 0) {
+        const categoryElement = event.target.parentElement.parentElement;
+        const choosedInCategory = categoryElement.querySelectorAll('.choosed');
+        const choosedCheckInCategory = categoryElement.querySelectorAll('.checkChoosed');
+        Array.from(choosedInCategory)
+          .map((element) => element.classList.remove('choosed'));
+        Array.from(choosedCheckInCategory)
+          .map((element) => element.classList.remove('checkChoosed'));
+      }
+      event.target.classList.toggle('choosed');
+      event.target.parentElement.firstChild.classList.toggle('checkChoosed');
+
+      activeClose();
+    }
+  }));
+};
+cardClickHandle();
+
+
+function cardCreator(tag = '', categoryRowIndex) {
+  const filteredData = {
+    ...data,
+    [Object.keys(data)[categoryRowIndex || 0]]: data[Object.keys(data)[categoryRowIndex || 0]].filter((item) => item.tags.includes(tag)),
+  }
+
+  const dataRendered = categoryRowIndex === undefined ? data : filteredData;
+
+  for (let index = 0; index < 3; index += 1) {
+    if (index !== categoryRowIndex && categoryRowIndex !== undefined) continue;
+    console.log(dataRendered);
+    for (let item = 0; item < dataRendered[Object.keys(dataRendered)[index]].length; item += 1) {
+
+      const thumb = dataRendered[Object.keys(dataRendered)[index]][item].thumb;
+      const title = dataRendered[Object.keys(dataRendered)[index]][item].title;
+      const description = dataRendered[Object.keys(dataRendered)[index]][item].description;
+      const price = dataRendered[Object.keys(dataRendered)[index]][item].price;
+
+      const cardElement = document.createElement('div');
+      const check = document.createElement('ion-icon');
+      const figureElement = document.createElement('figure');
+      const imgElement = document.createElement('img');
+      const titleElement = document.createElement('figcaption');
+      const descriptionElement = document.createElement('p');
+      const priceElement = document.createElement('p');
+      const cardMask = document.createElement('div');
+
+      cardElement.classList.add('card');
+      check.classList.toggle('checkMark');
+      figureElement.classList.add('card__figure');
+      cardMask.classList.add('card__mask');
+
+      cardElement.setAttribute('data-row', index);
+      imgElement.setAttribute('src', thumb);
+      check.setAttribute('name', 'checkmark-circle');
+      titleElement.innerHTML = title;
+      descriptionElement.innerHTML = description;
+      priceElement.innerHTML = `R$ ${price.toFixed(2)}`;
+
+      document.querySelector('.' + Object.keys(dataRendered)[index]).appendChild(cardElement);
+      cardElement.appendChild(check);
+      cardElement.appendChild(figureElement);
+      figureElement.appendChild(imgElement);
+      figureElement.appendChild(titleElement);
+      cardElement.appendChild(descriptionElement);
+      cardElement.appendChild(priceElement);
+      cardElement.appendChild(cardMask);
+    }
+  }
+  cardClickHandle();
+
+};
+cardCreator();
+
 const requestInfoBuilder = () => {
   const choosedCards = Array.from(document.querySelectorAll('.choosed'))
     .map((card) => card.parentElement);
@@ -96,44 +230,6 @@ const closeHandle = () => {
 
 };
 
-const activeClose = () => {
-  const choosed = Array.from(document.querySelectorAll('.choosed'));
-  const close = document.querySelector('.close');
-  const maxChoosed = 3;
-  if (choosed.length === maxChoosed) {
-    close.firstElementChild.removeAttribute('disabled');
-    close.firstElementChild.innerHTML = 'Fechar pedido';
-    close.firstElementChild.classList.add('closeActive');
-  } else {
-    close.firstElementChild.setAttribute('disabled', '');
-  }
-};
-
-const cardClickHandle = () => {
-  // transforma um HTMLCollection em algo tipo um array
-  // https://chuckdries.medium.com/traversing-the-dom-with-filter-map-and-arrow-functions-1417d326d2bc
-  const cards = Array.from(document.querySelectorAll('.card__mask'));
-
-  cards.map((card) => card.addEventListener('click', (event) => {
-    // lógica para os filhos da div não herdarem o evento
-    if (event.target === event.currentTarget) {
-      if (Array.from(document.querySelectorAll('.choosed')).length > 0) {
-        const categoryElement = event.target.parentElement.parentElement;
-        const choosedInCategory = categoryElement.querySelectorAll('.choosed');
-        const choosedCheckInCategory = categoryElement.querySelectorAll('.checkChoosed');
-        Array.from(choosedInCategory)
-          .map((element) => element.classList.remove('choosed'));
-        Array.from(choosedCheckInCategory)
-          .map((element) => element.classList.remove('checkChoosed'));
-      }
-      event.target.classList.toggle('choosed');
-      event.target.parentElement.firstChild.classList.toggle('checkChoosed');
-
-      activeClose();
-    }
-  }));
-};
-cardClickHandle();
 document.querySelector('.closeButton').addEventListener('click', () => closeHandle());
 
 const requestWhatsapp = () => {
--- driven-eats/scripts/interactiveScript.js
+++ driven-eats/scripts/interactiveScript.js
@@ -1,42 +1,92 @@
-import { data } from '../data.js';
+// import { data } from '../data.js';
 
-const cardCreator = () => {
-  for (let index = 0; index < Object.keys(data).length; index += 1) {
-    for (let item = 0; item < data[Object.keys(data)[index]].length; item += 1) {
-      const thumb = data[Object.keys(data)[index]][item].thumb;
-      const title = data[Object.keys(data)[index]][item].title;
-      const description = data[Object.keys(data)[index]][item].description;
-      const price = data[Object.keys(data)[index]][item].price;
+// function drinkFilterHandle({ target }) {
+//   const { value } = target;
+//   const tagsSelector = {
+//     'Filtrar': '',
+//     'Não Alcólico': '',
+//     'Cerveja': 'cerveja',
+//     'Vinhos': 'vinho',
+//     'Vinho Tinto': 'tinto',
+//     'Vinho Branco': 'branco',
+//   };
+//   const tag = tagsSelector[value];
 
-      const cardElement = document.createElement('div');
-      const check = document.createElement('ion-icon');
-      const figureElement = document.createElement('figure');
-      const imgElement = document.createElement('img');
-      const titleElement = document.createElement('figcaption');
-      const descriptionElement = document.createElement('p');
-      const priceElement = document.createElement('p');
-      const cardMask = document.createElement('div');
+//   const oldCards = document.querySelectorAll(`[data-row="${1}"]`);
+//   if (oldCards.length > 0) Array.from(oldCards).map((card) => card.remove());
 
-      cardElement.classList.add('card');
-      check.classList.toggle('checkMark');
-      figureElement.classList.add('card__figure');
-      cardMask.classList.add('card__mask');
+//   cardCreator(tag, 1);
+// }
 
-      imgElement.setAttribute('src', thumb);
-      check.setAttribute('name', 'checkmark-circle');
-      titleElement.innerHTML = title;
-      descriptionElement.innerHTML = description;
-      priceElement.innerHTML = `R$ ${price.toFixed(2)}`;
+// function dessertFilterHandle({ target }) {
+//   const { value } = target;
+//   const tagsSelector = {
+//     'Filtrar': '',
+//     'Chocolate': 'chocolate',
+//     'Mousses': 'mousse',
+//     'Bolos': 'bolo',
+//     'Pudins': 'pudim',
+//     'Doces': 'doce',
+//   };
+//   const tag = tagsSelector[value];
 
-      document.querySelector('.' + Object.keys(data)[index]).appendChild(cardElement);
-      cardElement.appendChild(check);
-      cardElement.appendChild(figureElement);
-      figureElement.appendChild(imgElement);
-      figureElement.appendChild(titleElement);
-      cardElement.appendChild(descriptionElement);
-      cardElement.appendChild(priceElement);
-      cardElement.appendChild(cardMask);
-    }
-  }
-};
-cardCreator();
+//   const oldCards = document.querySelectorAll(`[data-row="${2}"]`);
+//   if (oldCards.length > 0) Array.from(oldCards).map((card) => card.remove());
+//   cardCreator(tag, 2);
+// }
+
+// document.querySelector('#drinkFilters').addEventListener('click', drinkFilterHandle);
+// document.querySelector('#dessertFilters').addEventListener('click', dessertFilterHandle);
+
+
+// function cardCreator(tag = '', categoryRowIndex) {
+//   const filteredData = {
+//     ...data,
+//     [Object.keys(data)[categoryRowIndex || 0]]: data[Object.keys(data)[categoryRowIndex || 0]].filter((item) => item.tags.includes(tag)),
+//   }
+
+//   const dataRendered = categoryRowIndex === undefined ? data : filteredData;
+
+//   for (let index = 0; index < 3; index += 1) {
+//     if (index !== categoryRowIndex && categoryRowIndex !== undefined) continue;
+//     console.log(dataRendered);
+//     for (let item = 0; item < dataRendered[Object.keys(dataRendered)[index]].length; item += 1) {
+
+//       const thumb = dataRendered[Object.keys(dataRendered)[index]][item].thumb;
+//       const title = dataRendered[Object.keys(dataRendered)[index]][item].title;
+//       const description = dataRendered[Object.keys(dataRendered)[index]][item].description;
+//       const price = dataRendered[Object.keys(dataRendered)[index]][item].price;
+
+//       const cardElement = document.createElement('div');
+//       const check = document.createElement('ion-icon');
+//       const figureElement = document.createElement('figure');
+//       const imgElement = document.createElement('img');
+//       const titleElement = document.createElement('figcaption');
+//       const descriptionElement = document.createElement('p');
+//       const priceElement = document.createElement('p');
+//       const cardMask = document.createElement('div');
+
+//       cardElement.classList.add('card');
+//       check.classList.toggle('checkMark');
+//       figureElement.classList.add('card__figure');
+//       cardMask.classList.add('card__mask');
+
+//       cardElement.setAttribute('data-row', index);
+//       imgElement.setAttribute('src', thumb);
+//       check.setAttribute('name', 'checkmark-circle');
+//       titleElement.innerHTML = title;
+//       descriptionElement.innerHTML = description;
+//       priceElement.innerHTML = `R$ ${price.toFixed(2)}`;
+
+//       document.querySelector('.' + Object.keys(dataRendered)[index]).appendChild(cardElement);
+//       cardElement.appendChild(check);
+//       cardElement.appendChild(figureElement);
+//       figureElement.appendChild(imgElement);
+//       figureElement.appendChild(titleElement);
+//       cardElement.appendChild(descriptionElement);
+//       cardElement.appendChild(priceElement);
+//       cardElement.appendChild(cardMask);
+//     }
+//   }
+// };
+// cardCreator();
