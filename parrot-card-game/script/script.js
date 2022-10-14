d8816088594ac52880c2d3b2a694f5440398b173
Danilo Leão <olin_ad@hotmail.com>
2022-10-14 17:18:16 -0300

    validação do prompt

--- parrot-card-game/script/script.js
+++ parrot-card-game/script/script.js
@@ -1,7 +1,14 @@
 const initialGameConfig = () => {
-  const cardNumber = prompt('');
+  let cardNumber = prompt('Com quantas cartas quer começar? Escolha um número par de 4 a 14');
+
+  while (cardNumber % 2 !== 0 || (cardNumber < 4 || cardNumber > 14)) {
+    cardNumber = prompt('Com quantas cartas quer começar? Escolha um número par de 4 a 14');
+  }
+  console.log(cardNumber);
 };
 
 const cardCreator = () => {
 
 }
+
+window.addEventListener('load', initialGameConfig);
